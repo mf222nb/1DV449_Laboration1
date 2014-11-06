1e899aadfa1e128f5f4298d337211febc9bd8a93
mf222nb <mf222nb@student.lnu.se>
2014-11-06 18:00:46 +0100

    gjort så man kan hitta kursplanen och kurskoden för alla kurser

--- Scraper/index.php
+++ Scraper/index.php
@@ -1,24 +1,98 @@
 <?php
+ini_set('max_execution_time', 300);
+$data = curl_get_request("http://coursepress.lnu.se/kurser/");
 
-getAllCourses();
+$dom = new DOMDocument();
+$array = array();
 
-function getAllCourses(){
-    $data = curl_get_request("http://coursepress.lnu.se/kurser/");
+getAllCourses($dom ,$data, $array);
 
-    $dom = new DOMDocument();
+function getAllCourses($dom, $data, $array) {
+    $urlArray = $array;
 
     if($dom->loadHTML($data)){
         $xpath = new DOMXPath($dom);
-        $items = $xpath->query('//ul[@id = "blogs-list"]//div[@class = "item-title"]/a');
-
+        $items = $xpath->query('//ul[@id="blogs-list"]//div[@class="item-title"]/a');
         foreach($items as $item){
-            echo $item->nodeValue ." --> " . $item->getAttribute("href") . "<br />";
+            $courseCode = getCourseCode($dom,$item->getAttribute('href'));
+            $coursePlan = getCoursePlan($dom,$item->getAttribute('href'));
+            $courseEntryText = getCourseEntryText($dom,$item->getAttribute('href'));
+            $urlArray[] = "CourseName: " . $item->nodeValue . "--> Link:" . $item->getAttribute('href') . "--> CourseCode: " . $courseCode . "--> CoursePlan: " . $coursePlan ." --> Course Entry Text: " . $courseEntryText . "<br/>";;
+        }
+        echo "<br/>";
+        var_dump($urlArray);
+    }
+    getNextPage($dom, $data, $urlArray);
+}
+
+function getCourseEntryText($dom, $courseURL){
+    $courseURL = curl_get_request($courseURL);
+    libxml_use_internal_errors(true);
+    if($dom->loadHTML($courseURL)){
+        libxml_use_internal_errors(false);
+        $xpath = new DOMXPath($dom);
+
+        $courseEntryText = $xpath->query('//div[@class="entry-content"]/p/text()')->item(0);
+
+        if($courseEntryText != null){
+            return $courseEntryText->textContent;
+        }
+        else{
+            return "No entry text";
+        }
+
+    }
+}
+
+function getCourseCode($dom, $courseURL){
+    $courseURL = curl_get_request($courseURL);
+    libxml_use_internal_errors(true);
+    if($dom->loadHTML($courseURL)){
+        libxml_use_internal_errors(false);
+        $xpath = new DOMXPath($dom);
+        $courseCode = $xpath->query('//div[@id = "header-wrapper"]/ul/li[last()]/a/text()')->item(0);
+
+        if($courseCode != null){
+            return $courseCode->textContent;
+        }
+        else{
+            return "No course code";
         }
     }
-    else{
-        die("Fel vid inläsning av HTML");
-    }
+}
 
+function getCoursePlan($dom, $courseURL){
+    $courseURL = curl_get_request($courseURL);
+    libxml_use_internal_errors(true);
+    if($dom->loadHTML($courseURL)){
+        libxml_use_internal_errors(false);
+        $xpath = new DOMXPath($dom);
+        $coursePlan = $xpath->query('//ul[@class = "sub-menu"]/li/a/text()[contains(., "Kursplan")]')->item(0);
+
+        $href = $coursePlan->parentNode;
+        if($href != null){
+            return $href->getAttribute("href");
+        }
+        else{
+            return "No course plan";
+        }
+    }
+}
+
+function getNextPage($dom, $data , $urlArray){
+    if($dom->loadHTML($data)){
+
+        $xpath = new DOMXPath($dom);
+        $nextPageUrl = $xpath->query("//div[@id='pag-bottom']/div[@class='pagination-links']/a[@class='next page-numbers']");
+        foreach($nextPageUrl as $href){
+            $nextPageUrl =  $href->getAttribute('href') . "<br/>";
+        }
+
+        $nextPageUrl = curl_get_request("http://coursepress.lnu.se" . $nextPageUrl);
+        if(strlen($nextPageUrl) > 0){
+            getAllCourses($dom,$nextPageUrl,$urlArray);
+        }
+    }
 }
 
 function curl_get_request($url){
